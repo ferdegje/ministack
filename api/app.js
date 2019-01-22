1dc1ceeada5254226ace08ecf2db55b5d6469f75
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-22 06:27:48 +0000

    remove dead code

--- api/app.js
+++ api/app.js
@@ -12,9 +12,6 @@ var register = require('./routes/register')
 var validate = require('./routes/validate')
 var userDelete = require('./routes/userDelete')
 var userGet = require('./routes/userGet')
-var allKids = require('./routes/allKids')
-var allNews = require('./routes/allNews')
-var specificKid = require('./routes/specificKid')
 var genericCRUD = require('./routes/genericCRUD')
 var mediaUploadLink = require('./routes/mediaUploadLink')
 app.set('view engine', 'pug')
@@ -48,6 +45,9 @@ router.use(awsServerlessExpressMiddleware.eventContext())
 // NOTE: tests can't find the views directory without this
 app.set('views', path.join(__dirname, 'views'))
 
+router.get('/ping', (req, res) => {
+  res.json("pong")
+})
 router.get('/mediauploadlink', mediaUploadLink)
 router.post('/register', register)
 router.post('/validate', validate)
@@ -55,72 +55,7 @@ router.get('/user', userGet)
 router.delete('/user', userDelete)
 router.all('/:entityName([a-zA-Z0-9]+)', genericCRUD)
 router.all('/:entityName([a-zA-Z0-9]+)/:objectId([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', genericCRUD)
-// router.all('/gifts', allKids)
-// router.all('/gifts/:kidId([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', specificKid)
-// router.all('/news', allNews)
-// router.all('/news/:newsId([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})', allNews)
 
-router.get('/', (req, res) => {
-  res.render('index', {
-    apiUrl: req.apiGateway ? `https://${req.apiGateway.event.headers.Host}/${req.apiGateway.event.requestContext.stage}` : 'http://localhost:3000'
-  })
-})
-
-router.get('/sam', (req, res) => {
-  res.sendFile(`${__dirname}/sam-logo.png`)
-})
-
-router.get('/users', (req, res) => {
-  res.json(users)
-})
-
-router.get('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
-
-  if (!user) return res.status(404).json({})
-
-  return res.json(user)
-})
-
-router.post('/users', (req, res) => {
-  const user = {
-    id: ++userIdCounter,
-    name: req.body.name
-  }
-  users.push(user)
-  res.status(201).json(user)
-})
-
-router.put('/users/:userId', (req, res) => {
-  const user = getUser(req.params.userId)
-
-  if (!user) return res.status(404).json({})
-
-  user.name = req.body.name
-  res.json(user)
-})
-
-router.delete('/users/:userId', (req, res) => {
-  const userIndex = getUserIndex(req.params.userId)
-
-  if (userIndex === -1) return res.status(404).json({})
-
-  users.splice(userIndex, 1)
-  res.json(users)
-})
-
-const getUser = (userId) => users.find(u => u.id === parseInt(userId))
-const getUserIndex = (userId) => users.findIndex(u => u.id === parseInt(userId))
-
-// Ephemeral in-memory data store
-const users = [{
-  id: 1,
-  name: 'Joe'
-}, {
-  id: 2,
-  name: 'Jane'
-}]
-let userIdCounter = users.length
 
 // The aws-serverless-express library creates a server and listens on a Unix
 // Domain Socket for you, so you can remove the usual call to app.listen.
