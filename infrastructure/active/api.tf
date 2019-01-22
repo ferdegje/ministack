ecb0ef9118960eacbbaccba075947f813b932577
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-22 05:01:01 +0000

    expose url of API

--- infrastructure/active/api.tf
+++ infrastructure/active/api.tf
@@ -112,3 +112,7 @@ resource "aws_cloudwatch_log_group" "example" {
   name              = "/aws/lambda/${aws_lambda_function.example.function_name}"
   retention_in_days = 14
 }
+
+output "api_url" {
+  value = "https://${aws_api_gateway_domain_name.example}/"
+}
