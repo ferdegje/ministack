a98105ab6e93b8f50461e3a41a0bd979ce3c478b
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-28 04:35:13 +0000

    keep the bucket at night

---
+++ infrastructure/dormant/s3.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "www" {
+  bucket = "${local.project}.${local.domain}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+  }
+}
