ab4b9101316c0778d4138a31ede787f6d2339a0a
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-28 04:24:29 +0000

    add dns

---
+++ infrastructure/active/route53.tf
@@ -0,0 +1,15 @@
+data "aws_route53_zone" "selected" {
+  name         = "${local.domain}."
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = "${aws_route53_zone.selected.zone_id}"
+  name    = "${local.project}.${local.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_s3_bucket.www.bucket_domain_name}"
+    zone_id                = "${aws_s3_bucket.www.hosted_zone_id}"
+    evaluate_target_health = true
+  }
+}
