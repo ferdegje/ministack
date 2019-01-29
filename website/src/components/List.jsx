30f6bf0555e765246e202edc7534e33f67844653
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-29 22:06:59 +0000

    react

--- website/src/components/List.jsx
+++ website/src/components/List.jsx
@@ -1,6 +1,8 @@
 // src/components/List.jsx
 import React from "react";
 import { connect } from "react-redux";
+import { BrowserRouter as NavLink } from "react-router-dom";
+
 const mapStateToProps = state => {
   return { articles: state.articles };
 };
@@ -8,7 +10,8 @@ const ConnectedList = ({ articles }) => (
   <ul className="list-group list-group-flush">
     {articles.map(el => (
       <li className="list-group-item" key={el.id}>
-        {el.title}
+        <NavLink to={`/home/${el.id}`} className="nav-link">{el.title}</NavLink>
+        
       </li>
     ))}
   </ul>
