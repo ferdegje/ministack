7627b6e864b08b3d1307e3f593b25269a58dc741
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-01-31 05:39:02 +0000

    capture details

--- website/src/views/UserLogin/UserLogin.jsx
+++ website/src/views/UserLogin/UserLogin.jsx
@@ -30,115 +30,153 @@ const styles = {
   }
 };
 
-function UserLogin(props) {
-  const { classes } = props;
-  return (
-    <div>
-      <GridContainer>
-        <GridItem xs={12} sm={12} md={8}>
+
+class UserLogin extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.myRef = React.createRef();
+    this.inputProps = {
+      onChange: this.inputChanged
+    }
+  }
+
+  registerBtnClick = () => {
+    console.log("hello", this.state);
+  }
+
+  inputChanged = (evt) => {
+    // this.state.email = evt.target.value
+    let obj = {}
+    obj[evt.target.id] = evt.target.value
+    this.setState(obj)
+  }
+
+  handleFieldChange = (field) => (event, value, selectedKey) => {
+    // make a copy of the object first to avoid changes by reference
+    let data = { ...this.state.data };
+    console.log(value)
+    // use here event or value of selectedKey depending on your component's event
+    data[field] = value;
+    this.setState({ data });
+  }
+
+  render() {
+    return (
+      <div>
+        <GridContainer>
+          <GridItem xs={12} sm={12} md={8}>
+            <Card>
+              <CardHeader color="primary">
+                <h4 className={this.props.classes.cardTitleWhite}>Register</h4>
+                <p className={this.props.classes.cardCategoryWhite}>Create your account</p>
+              </CardHeader>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <CustomInput
+                      labelText="Email address"
+                      id="email-address"
+                      inputProps={this.inputProps}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                      
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="First Name"
+                      id="first-name"
+                      inputProps={this.inputProps}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={6}>
+                    <CustomInput
+                      labelText="Last Name"
+                      id="last-name"
+                      inputProps={this.inputProps}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <CustomInput
+                      labelText="Password"
+                      id="password"
+                      inputProps={this.inputProps}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={8}>
+                    <CustomInput
+                      labelText="Personal quote"
+                      id="personal-quote"
+                      inputProps={this.inputProps}
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <Button color="primary" onClick={this.registerBtnClick}>Create Account</Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+          <GridItem xs={12} sm={12} md={4}>
           <Card>
-            <CardHeader color="primary">
-              <h4 className={classes.cardTitleWhite}>Register</h4>
-              <p className={classes.cardCategoryWhite}>Create your account</p>
-            </CardHeader>
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="First Name"
-                    id="first-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={6}>
-                  <CustomInput
-                    labelText="Last Name"
-                    id="last-name"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={4}>
-                  <CustomInput
-                    labelText="Password"
-                    id="password"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-                <GridItem xs={12} sm={12} md={8}>
-                  <CustomInput
-                    labelText="Personal quote"
-                    id="personal-quote"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <Button color="primary">Create Account</Button>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={12} md={4}>
-        <Card>
-            <CardHeader color="info">
-              <h4 className={classes.cardTitleWhite}>Login</h4>
-              <p className={classes.cardCategoryWhite}>Use your existing credentials</p>
-            </CardHeader>
-            <CardBody>
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <CustomInput
-                    labelText="Email address"
-                    id="email-address"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
-              </GridContainer>
-              
-              <GridContainer>
-                <GridItem xs={12} sm={12} md={12}>
-                  <CustomInput
-                    labelText="Password"
-                    id="password"
-                    formControlProps={{
-                      fullWidth: true
-                    }}
-                  />
-                </GridItem>
+              <CardHeader color="info">
+                <h4 className={this.props.classes.cardTitleWhite}>Login</h4>
+                <p className={this.props.classes.cardCategoryWhite}>Use your existing credentials</p>
+              </CardHeader>
+              <CardBody>
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <CustomInput
+                      labelText="Email address"
+                      id="email-address"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                </GridContainer>
                 
-              </GridContainer>
-            </CardBody>
-            <CardFooter>
-              <Button color="info">Log in</Button>
-            </CardFooter>
-          </Card>
-        </GridItem>
-      </GridContainer>
-    </div>
-  );
+                <GridContainer>
+                  <GridItem xs={12} sm={12} md={12}>
+                    <CustomInput
+                      labelText="Password"
+                      id="password"
+                      formControlProps={{
+                        fullWidth: true
+                      }}
+                    />
+                  </GridItem>
+                  
+                </GridContainer>
+              </CardBody>
+              <CardFooter>
+                <Button color="info">Log in</Button>
+              </CardFooter>
+            </Card>
+          </GridItem>
+        </GridContainer>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(UserLogin);
