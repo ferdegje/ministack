78f6e46605cc05a33ce81e6ed1a5a0b3f81ae091
Jean-Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2019-02-21 20:38:53 +0000

    remove useless argument

--- infrastructure/active/api.tf
+++ infrastructure/active/api.tf
@@ -75,7 +75,6 @@ resource "aws_api_gateway_integration" "lambda_root" {
 
 module "MyResourceCors" {
   source = "github.com/carrot/terraform-api-gateway-cors-module"
-  resource_name = "MyResource"
   resource_id = "${aws_api_gateway_resource.proxy.id}"
   rest_api_id = "${aws_api_gateway_rest_api.example.id}"
 }
