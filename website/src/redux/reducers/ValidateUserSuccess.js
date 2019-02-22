b8d6e18a0574fc2e1c89b00e440fea68d85712b3
Jean-Marie Ferdegue <otransit@hotmail.com>
2019-02-22 13:34:54 +0000

    linting fail

--- website/src/redux/reducers/ValidateUserSuccess.js
+++ website/src/redux/reducers/ValidateUserSuccess.js
@@ -1,12 +1,6 @@
 import * as strings from "../actionsString"
 const initialState = {
 }
-function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + (exdays*24*60*60*1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
 
 function expireCookie(cname) {
     document.cookie = cname+"= ; expires = Thu, 01 Jan 1970 00:00:00 GMT"
