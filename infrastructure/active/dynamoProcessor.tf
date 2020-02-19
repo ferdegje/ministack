68c854811c75e4ae0aa403cd3eac7a150eb03a79
Jean Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2020-02-19 05:25:29 +0000

    fix version of handler

--- infrastructure/active/dynamoProcessor.tf
+++ infrastructure/active/dynamoProcessor.tf
@@ -10,7 +10,7 @@ resource "aws_lambda_function" "dynamo_processor" {
   role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "lambda.handler"
   source_code_hash = "${base64sha256(file("../../dynamo_processor/lambda_function_payload.zip"))}"
-  runtime          = "nodejs8.10"
+  runtime          = "nodejs12.x"
   timeout          = 15
   memory_size      = 3008
 
