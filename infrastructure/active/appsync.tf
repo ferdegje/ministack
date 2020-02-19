c13f1aa310d3858e85c7cc4ce12987dfa0bdd256
Jean Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2020-02-19 05:38:34 +0000

    set appsync datasource to dynamo

--- infrastructure/active/appsync.tf
+++ infrastructure/active/appsync.tf
@@ -22,13 +22,55 @@ schema {
 EOF
 }
 
-resource "aws_appsync_datasource" "test" {
-  api_id      = "${aws_appsync_graphql_api.test.id}"
-  name        = "ex"
-  type        = "HTTP"
+resource "aws_iam_role" "example" {
+  name = "example"
 
-  http_config {
-    endpoint = "http://example.com"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "appsync.amazonaws.com"
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "example" {
+  name = "example"
+  role = "${aws_iam_role.example.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "dynamodb:*"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "${aws_dynamodb_table.article.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_appsync_datasource" "example" {
+  api_id           = "${aws_appsync_graphql_api.test.id}"
+  name             = "tf_appsync_example"
+  service_role_arn = "${aws_iam_role.example.arn}"
+  type             = "AMAZON_DYNAMODB"
+
+  dynamodb_config {
+    table_name = "${aws_dynamodb_table.article.name}"
   }
 }
 
