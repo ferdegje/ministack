e35215d87bd2f78be924cdd83fdc5c2eae747971
Jean Marie Ferdegue <jeanmarie.ferdegue@gmail.com>
2020-03-10 05:17:10 +0000

    generates uuid

--- website/components/Submit.js
+++ website/components/Submit.js
@@ -1,15 +1,13 @@
 import { useMutation } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import { ALL_POSTS_QUERY, allPostsQueryVars } from './PostList'
+import { v4 as uuidv4 } from 'uuid';
 
 const CREATE_POST_MUTATION = gql`
-  mutation createPost($title: String!, $url: String!) {
-    createPost(title: $title, url: $url) {
+  mutation createMinistackArticle($createministackarticleinput: CreateMinistackArticleInput!) {
+    createMinistackArticle(input: $createministackarticleinput) {
       id
       title
-      votes
-      url
-      createdAt
     }
   }
 `
@@ -22,12 +20,18 @@ export default function Submit() {
     const form = event.target
     const formData = new window.FormData(form)
     const title = formData.get('title')
-    const url = formData.get('url')
+    const id = uuidv4()
     form.reset()
 
     createPost({
-      variables: { title, url },
+      variables: { 
+        createministackarticleinput: {
+          id: id,
+          title: title
+        }
+      },
       update: (proxy, { data: { createPost } }) => {
+        console.log(data);
         const data = proxy.readQuery({
           query: ALL_POSTS_QUERY,
           variables: allPostsQueryVars,
@@ -37,7 +41,14 @@ export default function Submit() {
           query: ALL_POSTS_QUERY,
           data: {
             ...data,
-            allPosts: [createPost, ...data.allPosts],
+            listMinistackArticles: {
+              ...data.listMinistackArticles,
+              items: [{
+                id: id,
+                title: title,
+                __typename: "hello"
+              },...data.listMinistackArticles.items]
+            }
           },
           variables: allPostsQueryVars,
         })
@@ -49,7 +60,6 @@ export default function Submit() {
     <form onSubmit={handleSubmit}>
       <h1>Submit</h1>
       <input placeholder="title" name="title" type="text" required />
-      <input placeholder="url" name="url" type="url" required />
       <button type="submit" disabled={loading}>
         Submit
       </button>
