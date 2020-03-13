bec0dd9774ba61fb65e26578f208b3c71f71ee98
Jean Marie Ferdegue <jeanmarie.ferdegue@b-2358.lan>
2020-03-13 06:19:55 +0000

    retrieve JSON

--- website/apolloClient.js
+++ website/apolloClient.js
@@ -2,11 +2,13 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
-import Infrastructure from './infrastructure.json'
 
 export default function createApolloClient(initialState, ctx) {
   // The `ctx` (NextPageContext) will only be present on the server.
   // use it to extract auth headers (ctx.req) or similar.
+  let response = await fetch('https://infrastructure.ministack.nokdo.com/infrastructure.json');
+  let Infrastructure = await response.json();
+
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
