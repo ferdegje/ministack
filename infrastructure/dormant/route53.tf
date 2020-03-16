499e544d643b14d082bcf39856d2ad9c671d6350
Jean Marie Ferdegue <jeanmarie.ferdegue@b-2358.lan>
2020-03-16 06:23:13 +0000

    Presents domain data

--- infrastructure/dormant/route53.tf
+++ infrastructure/dormant/route53.tf
@@ -1,3 +1,7 @@
+data "aws_route53_zone" "selected" {
+  name = "${local.domain}."
+}
+
 resource "aws_route53_record" "www-dev" {
   zone_id = data.aws_route53_zone.selected.zone_id
   name    = local.project
