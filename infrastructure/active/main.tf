6a50bf090e480fb1d9dccb7a319f5c8dcbef3081
Jean Marie Ferdegue <jeanmarie.ferdegue@b-2358.lan>
2020-04-06 14:14:01 +0100

    Extract project info from TF codebase

--- infrastructure/active/main.tf
+++ infrastructure/active/main.tf
@@ -17,15 +17,31 @@ terraform {
 }
 
 locals {
-  region          = "eu-west-2"
-  accountId       = "314027424334"
-  domain          = "nokdo.com"
-  project         = "ministack"
-  projectLongName = "Ministack demo"
+  region          = var.region
+  accountId       = var.accountId
+  domain          = var.domain
+  project         = var.project
+  projectLongName = var.projectLongName
+}
+
+variable "region" {
+  default = ""
+}
+variable "accountId" {
+  default = ""
+}
+variable "domain" {
+  default = ""
+}
+variable "project" {
+  default = ""
+}
+variable "projectLongName" {
+  default = ""
 }
 
 output "region" {
-  value = local.region
+  value = var.region
 }
 
 output "projectName" {
