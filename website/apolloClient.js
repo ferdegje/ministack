29b1a6d892931d5ca385dd764ef3ddbe698a521d
Jean Marie Ferdegue <jeanmarie.ferdegue@b-2358.lan>
2020-04-06 10:37:35 +0100

    Typo fix

--- website/apolloClient.js
+++ website/apolloClient.js
@@ -4,16 +4,19 @@ import { HttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
 
 export default function createApolloClient(initialState, ctx) {
-  // let uri = "https://7w4lgaz3p5gctlehfdrvog5kqe.appsync-api.eu-west-2.amazonaws.com/graphql"
-  let uri = "https://graphql-ministack.nokdo.com/graphql"
+  let uri
+  uri = "https://7w4lgaz3p5gctlehfdrvog5kqe.appsync-api.eu-west-2.amazonaws.com/graphql"
+  uri = "https://graphql-ministack.nokdo.com/graphql"
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
       uri: uri, // Server URL (must be absolute)
       headers: {
-        "x-api-key": "da2-a2qs4xbs25h5rl3zo4dp57b76u"
+        "x-api-key": "da2-a2qs4xbs25h5rl3zo4dp57b76u",
+        "Origin": "localhost:3000"
       },
       fetch,
+      credentials: "include"
     }),
     cache: new InMemoryCache().restore(initialState),
   })
--- website/components/Header.js
+++ website/components/Header.js
@@ -7,7 +7,7 @@ const Header = ({ router: { pathname } }) => (
       rel="stylesheet"
       href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
       integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-      crossorigin="anonymous"
+      crossOrigin="anonymous"
     />
     <Link href="/">
       <a className={pathname === '/' ? 'is-active' : ''}>Home</a>
