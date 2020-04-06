542773b66ae4fcd42fc37c64090868b2adc31026
Jean Marie Ferdegue <jeanmarie.ferdegue@b-2358.lan>
2020-04-06 17:58:42 +0100

    use all project information from one source

--- website/apolloClient.js
+++ website/apolloClient.js
@@ -2,12 +2,13 @@ import { ApolloClient } from 'apollo-client'
 import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
+import MiniStack from './project.tfvars.json'
 
 export default function createApolloClient(initialState, ctx) {
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
-      uri: "https://graphql-ministack.nokdo.com/graphql", // Server URL (must be absolute)
+      uri: "https://graphql-"+MiniStack.project+"."+MiniStack.domain+"/graphql", // Server URL (must be absolute)
       headers: {
         "x-api-key": "da2-vfxzgqrp6fhpxkod2asjlctvee",
         "Cache-Control": "no-cache"
@@ -18,5 +19,4 @@ export default function createApolloClient(initialState, ctx) {
     }),
     cache: new InMemoryCache().restore(initialState),
   })
-  console.log("tto")
 }
